01831aab0648df69c9dfeebb6f8a9d7a4ac41f84
MaximPoz <executioner2126@gmail.com>
2022-12-31 12:14:40 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.scss';
+import { Success } from './components/Success';
+import { Users } from './components/Users';
+
+// Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = React.useState([]);
+  const [isLoading, setLoading] = React.useState(true);
+  const [invites, setInvites] = React.useState([]);
+  const [success, setSuccess] = React.useState(false);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  React.useEffect(() => {
+    fetch('https://reqres.in/api/users') //взять от сюда
+      .then((res) => res.json()) //конвертировать в json
+      .then((json) => {       //из json забрать 
+        setUsers(json.data)  // data (там все пользователи)
+      })
+      .catch((err) => { //Если будет ошибка то и так понятно
+        console.warn(err)
+        alert('Ошибка при получении пользователей')
+      })
+      .finally(() => setLoading(false))//когда загрузка закончилась, вырубить Loading
+  }, []);
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  }
+
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites((prev) => prev.filter((_id) => _id !== id))
+    } else {
+      setInvites((prev) => [...prev, id])
+    }
+  }
+
+  const onClickSendInvites = () => {
+    setSuccess(true);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {success ? (
+        <Success count={invites.length}/>
+      ) : (
+        <Users
+          onChangeSearchValue={onChangeSearchValue}
+          searchValue={searchValue}
+          items={users}
+          isLoading={isLoading}
+          onClickInvite={onClickInvite}
+          invites={invites}
+          onClickSendInvites={onClickSendInvites}
+
+        />
+      )}
+
     </div>
   );
 }
